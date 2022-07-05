f34ef3adaf87e30827a9e0c8614f13994fd2f42a
Hangrui Bi <hangruibi@outlook.com>
2022-07-05 17:26:53 +0800

    removed unnecessary usage of tensorflow

--- utils.py
+++ utils.py
@@ -1,12 +1,11 @@
 import torch
-import tensorflow as tf
 import os
 import logging
-
+from pathlib import Path
 
 def restore_checkpoint(ckpt_dir, state, device):
-  if not tf.io.gfile.exists(ckpt_dir):
-    tf.io.gfile.makedirs(os.path.dirname(ckpt_dir))
+  if not os.path.isdir(ckpt_dir):
+    Path(os.path.dirname(ckpt_dir)).mkdir(parents=True, exist_ok=True)
     logging.warning(f"No checkpoint found at {ckpt_dir}. "
                     f"Returned the same state as input")
     return state
